1d8e035b5337deb74fb59c1013d8e60c1cd6683d
Taylor Clayton <tclayton@uoregon.edu>
2020-04-25 11:45:30 -0700

    Add functions to functions.js

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,5 +1,40 @@
 // jshint esversion: 6
 
+//Req. 3-A
+
+let revWords1 = sentence => {
+  let words = sentence.split("");
+  let newSentence = " ";
+  for (var i = 0; i < words.length; i++) {
+    newSentence = words[i] + newSentence;
+  }
+  return newSentence;
+};
+
+//Req. 3-B
+
+let revWords2 = sentence => {
+  let words = sentence.split("");
+  let newSentence = " ";
+  words.forEach(function(word) {
+    newSentence = word + newSentence;
+  });
+  return newSentence;
+};
+
+//Req. 3-C
+
+let revWords3 = sentence => {
+  let words = sentence.split("");
+  let newSentence = " ";
+  for (var element of words) {
+    newSentence = element + newSentence;
+  }
+
+  return newSentence;
+};
+
+//Req. 5, XC
 let containsDuplicates = s => {
   let cArr = [0, 0, 0, 0],
     code;
